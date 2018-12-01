ea9d709c0817612cac0593ce3f9c92e200280bdc
ashd32 <ashd32@gmail.com>
2018-12-01 13:51:18 -0600

    added search input

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,34 +10,15 @@ module.exports = function (app) {
     });
   });
 
-  app.get('/api/products/:id', function (req, res) {
-    db.Product.findALL({
+  app.get('/api/products/:department_name', function (req, res) {
+    db.Product.findAll({
       where: {
-        id: req.params.id
+        department_name: req.params.department_name
       }
     }).then(function (data) {
       res.json(data);
     }).catch(function (err) {
       res.json(err);
     })
-  });
-
-  app.post('/api/products', function (req, res) {
-    db.Product.create(req.body).then(function (rows) {
-      res.json({ success: true });
-    }).catch(function (error) {
-      res.json({ error: error })
-    });
-  });
-
-  app.put('/api/products/:id', function (req, res) {
-    db.Product.update(
-      req.body,
-      { where: { id: req.params.id } }
-    ).then(function () {
-      res.json({ success: true });
-    }).catch(function (error) {
-      res.json({ error: error });
-    });
   });
 }
