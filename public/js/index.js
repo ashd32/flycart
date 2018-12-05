8d433fe98a94bc87c1dc1deb8b94a667d190602f
Ashley Donald <ashd32@gmail.com>
2018-12-05 18:19:24 -0500

    added modal functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 let productArray = [];
+let shoppingCart = [];
 
 
 // render function to display data
@@ -30,18 +31,41 @@ searchMerch();
     console.log(productArray);
   const productId = parseInt($('#productId').val())
   const productQuantity = parseInt($('#productQuantity').val());
+  let merchStock = 0;
 
+  // identifies correct product
 for(let i=0; i< productArray.length; i++){
     if(productId === productArray[i].id){
+        // checks if quantity is sufficient
        if(productQuantity <= productArray[i].stock_quantity){
-        merchStock = productArray[i].stock_quantity - productQuantity;
+        // merchStock = productArray[i].stock_quantity - productQuantity;
         // console.log("Items are available!");
+        const id = productArray[i].id;
+        const product_name = productArray[i].product_name;
+        const price = productArray[i].price;
+        const stock_quantity = productArray[i].stock_quantity;
+        shoppingCart.push({id:id, product_name:product_name, product_quantity:productQuantity, price:price, stock_quantity:stock_quantity})  // holds value user selected
+        alert(productArray[i].product_name + ' was added to your shopping cart!')
        } else {
            alert("Items are out of stock");
        }
     }
 }
 
-  console.log(productId, productQuantity);  
+  console.log(productId, merchStock);  
+})
+
+// listener for shopping cart array
+$("#shopping-cart").click(function(event){
+    event.preventDefault();
+    if(shoppingCart.length === 0){
+        alert('Your shopping cart is empty')
+    }else {
+        console.log(shoppingCart);
+    $('.modal').css('display', 'block') // changes modal to display
+    }
+})
+$('.close').click(function(){
+    $('.modal').css('display', 'none') // changes modal to close
 })
 });
