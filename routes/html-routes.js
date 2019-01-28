a86e001f195f3a6769c7ce6ab5ba19c57ab1ff5f
Ashley Donald <ashd32@gmail.com>
2019-01-28 17:39:56 -0600

    cleans up code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,14 +15,6 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get('/results', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/results.html'));
-  });
-
-  app.get('/posting', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/posting.html'));
-  });
-
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
